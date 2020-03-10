f640b3a0ab8bcb0bddfcebcb70135a71c46b1358
vetlekallak <vetlekallak@gmail.com>
2020-03-10 23:22:11 +0100

    endret litt på week11 plot

--- javascript/index.js
+++ javascript/index.js
@@ -56,7 +56,6 @@ window.onload = function() {
         selector: '#snusjobb',
         type: 'column',
         settings: {
-            title: 'Snus',
             colors: ['purple'],
             bar: {groupWidth: '50%'},
             vAxis: {title: 'Andel som har fått jobb', minValue: 0, maxValue: 100},
